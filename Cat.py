60caa509640aa4636740f89d9c1bfb080a1f8b08
unknown <ok.brede@inbox.lv>
2022-04-19 14:05:28 +0300

    A practicum-16.7 is added

---
+++ Cat.py
@@ -0,0 +1,11 @@
+from Pet import PetCase
+
+cat_1 = PetCase("Tom", "boy", 3)
+print("Cat_1: ", cat_1.name)
+print("Cat_1: ", cat_1.gender)
+print("Cat_1: ", cat_1.age)
+
+cat_2 = PetCase("Lika", "girl", 5)
+print("Cat_2: ", cat_2.name)
+print("Cat_2: ", cat_2.gender)
+print("Cat_2: ", cat_2.age)
